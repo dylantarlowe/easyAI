c3dd991647047d106747c80c52bdb79b13a5b57e
Dylan Tarlowe <dylantarlowe>
2023-04-16 13:22:11 -0400

    adding logs

--- src/pages/api/mongo/getUser.js
+++ src/pages/api/mongo/getUser.js
@@ -6,30 +6,36 @@ async function handler(req, res) {
   if (req.method !== 'POST') {
     return;
   }
-  const data = req.body;
-  const email = data.email;
+  try {
+    const data = req.body;
+    const email = data.email;
 
-  const client = await connectToDatabase();
+    const client = await connectToDatabase();
 
-  if (client) {
-    console.log("client is connected")
+    if (client) {
+      console.log("client is connected")
+    }
+    else {
+      console.log("client is not connected")
+    }
+
+    const db = client.db('easyAI')  
+
+    const existingUser = await db.collection('users').findOne({ email: email }) 
+
+
+    if (existingUser) {
+      console.log("existingUser is true")
+      res.status(201).json({ existingUser });
+      return;
+    }
+    else {
+      res.status(422).json({ message: 'user not found' });
+      return;
+    }
   }
-  else {
-    console.log("client is not connected")
-  }
-
-  const db = client.db('easyAI')  
-
-  const existingUser = await db.collection('users').findOne({ email: email }) 
-
-
-  if (existingUser) {
-    console.log("existingUser is true")
-    res.status(201).json({ existingUser });
-    return;
-  }
-  else {
-    res.status(422).json({ message: 'user not found' });
+  catch(e) {
+    console.log("error!"  + e)
     return;
   }
 }
